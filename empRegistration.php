2eb207e30aefe126c7422c38f97b9a1ed1dec628
nishagandhi1311 <73732638+nishagandhi1311@users.noreply.github.com>
2020-10-31 12:47:32 +0530

    JQuery validation on form

--- empRegistration.php
+++ empRegistration.php
@@ -5,13 +5,83 @@
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+            <script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.js"></script>
+            <script>
+                $(document).ready(function()
+                {
+                    $("#form_register").validate(
+                    {
+                        rules:{
+                            name:{
+                                required: true
+                            },
+                            phoneNo:{
+                                required: true,
+                                minlength:10,
+                                maxlength:10,
+                                number: true
+                            },
+                            email:{
+                                required:true,
+                                email:true
+                            },
+                            age:{
+                                required: true,
+                                minlength:2,
+                                maxlength:2,
+                                number: true
+                            },
+                            experience: {
+                                required: {
+                                    depends: function(element){
+                                    if('none' == $('#experience').val()){
+                                        $('#experience').val('');
+                                    }
+                                    return true;
+                                }
+                                }
+                            },
+                            "skill[]": {
+                                required: true,
+                                minlength: 1
+                            },
+                            fileupload: {
+                                required: true
+                            }
+                        },
+                        messages:{
+                            name:{
+                                required: "Please Enter Name",
+                            },
+                            phoneNo:{
+                                required: "Please Enter Phone No",
+                            },
+                            email:{
+                                required: "Please Enter Email",
+                            },
+                            age:{
+                                required: "Please Enter Age",
+                            },
+                            experience: {
+                                required: "Please select experience",
+                            },
+                            "skill[]": {
+                                required: "Please select skill",
+                            },
+                            fileupload: {
+                                required: "please select file"
+                            }
+                        }
+                    });
+                });
+            </script>
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <h3>Application Form</h3>
                 <div class="col-sm-8">
-                    <form method="POST" enctype="multipart/form-data">
+                    <form method="POST" enctype="multipart/form-data" id="form_register">
                         <div class="form-group">
                             <label for="name">Name:</label>
                             <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
@@ -30,8 +100,8 @@
                         </div>
                         <div class="form-group">
                             <label for="experience">Experience:</label>
-                            <select name="experience" class="form-control">
-                                <option value="#">Please select Experience</option>
+                            <select name="experience" class="form-control" id="experience">
+                                <option value="none">Please select Experience</option>
                                 <option value="0">0</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
@@ -43,10 +113,10 @@
                         <div class="form-group">
                             <label for="skill">Skill:</label>
                             <div class="checkbox" name="skill">
-                                <label><input type="checkbox" name="sports">Sports</label>
-                                <label><input type="checkbox" name="reading">Reading</label>
-                                <label><input type="checkbox" name="programming">Programming</label>
-                                <label><input type="checkbox" name="dance">Dance</label>
+                                <label><input type="checkbox" name="skill[]" value="sports">Sports</label>
+                                <label><input type="checkbox" name="skill[]" value="reading">Reading</label>
+                                <label><input type="checkbox" name="skill[]" value="programming">Programming</label>
+                                <label><input type="checkbox" name="skill[]" value="dance">Dance</label>
                             </div>
                         </div>
                         <div class="input-group">
@@ -54,7 +124,7 @@
                                 <label for="resume">Resume:</label>
                             </div>
                             <div class="custom-file">
-                                <input type="file" class="form-control" id="fileupload">
+                                <input type="file" class="form-control" name="fileupload" id="fileupload">
                             </div>
                         </div>
                             <button type="submit" class="btn btn-success">Submit</button>
